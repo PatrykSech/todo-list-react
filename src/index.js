db707bbed16fad02960618558706602689874d01
Patryk Sech <Patrykwot22@wp.pl>
2023-01-31 20:13:33 +0100

    GlobalStyle added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* <GlobalStyle /> */}
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
