e9becb47a4d9b373f16b881fc79b993eb25065e3
Patryk Sech <Patrykwot22@wp.pl>
2023-03-08 00:06:11 +0100

    create simple nav

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
-import Tasks from "./features/tasks/Tasks";
+import TasksPage from "./features/tasks/tasksPage";
+import TaskPage from "./features/tasks/taskPage";
+import { NavLink, HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter basename="/todos-list-react">
+    <nav>
+        <ul>
+            <li>
+                <NavLink activeClassName="active" to="/zadania">Zadania</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName="active" to="/autor">O autorze</NavLink>
+            </li>
+        </ul>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </nav>
+    </HashRouter>
+)
