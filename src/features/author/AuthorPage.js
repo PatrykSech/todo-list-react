bfad012ac06dcf6945329912cbd435ddea62cd15
Patryk Sech <Patrykwot22@wp.pl>
2023-03-08 00:06:28 +0100

    create atour page

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,12 @@
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+export default () => (
+    <>
+    <Header title="O autorze" />
+    <Section
+    title="Patryk Sech"
+    body={<>Lorem ipsum</>}
+    />
+    </>
+);
